591bc5260cc250d2fc3b86b524586079dd5b30e2
sdelamo <sergio.delamo@softamo.com>
2017-01-10 07:19:57 +0100

    Initiat output of create-guide.sh script

---
+++ complete/grails-app/controllers/building/an/android/client/powered/by/a/grails/backend/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package building.an.android.client.powered.by.a.grails.backend
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ initial/grails-app/init/building/an/android/client/powered/by/a/grails/backend/BootStrap.groovy
@@ -0,0 +1,9 @@
+package building.an.android.client.powered.by.a.grails.backend
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
